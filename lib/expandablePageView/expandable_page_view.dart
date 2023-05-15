928e3d374b537aab1505175da77178eb5e9c2a77
yudongliang <yudongliang@apusapps.com>
2023-05-15 20:52:21 +0800

    卡片ViewPage使用

---
+++ lib/expandablePageView/expandable_page_view.dart
@@ -0,0 +1,82 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_application/expandablePageView/page/balance_page.dart';
+import 'package:flutter_demo_application/expandablePageView/page/basic_examples_page.dart';
+import 'package:flutter_demo_application/expandablePageView/page/vertical_balance_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          BalancePage(),
+          VerticalBalancePage(),
+          BasicExamplesPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        currentIndex: _currentIndex,
+        unselectedItemColor: const Color(0xff3a0ca3),
+        selectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.attach_money_rounded,
+            ),
+            label: '首页',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.currency_exchange_rounded,
+            ),
+            label: '百宝箱',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.info,
+            ),
+            label: '我的',
+          ),
+        ],
+        onTap: (index) => setState(() => _currentIndex = index),
+      ),
+    );
+  }
+}
---
+++ lib/expandablePageView/page/vertical_balance_page.dart
@@ -0,0 +1,63 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_application/expandablePageView/page/view/small_info_tile.dart';
+import 'package:flutter_demo_application/expandablePageView/page/view/widgets/small_deposit_button.dart';
+import 'package:flutter_demo_application/expandablePageView/page/view/widgets/small_withdraw_button.dart';
+
+class VerticalBalancePage extends StatelessWidget {
+  const VerticalBalancePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ExpandablePageView(
+            alignment: Alignment.centerLeft,
+            scrollDirection: Axis.vertical,
+            children: const [
+              SmallInfoTile(
+                icon: Icon(
+                  Icons.euro,
+                  size: 36,
+                  color: Colors.white,
+                ),
+                text: '54,00',
+              ),
+              SmallInfoTile(
+                icon: Icon(
+                  Icons.currency_pound,
+                  size: 36,
+                  color: Colors.white,
+                ),
+                text: '4.457,00',
+              ),
+              SmallInfoTile(
+                icon: Icon(
+                  Icons.currency_bitcoin,
+                  size: 36,
+                  color: Colors.white,
+                ),
+                text: '14,1230044',
+              ),
+            ],
+          ),
+          const SizedBox(width: 8),
+          Flexible(
+            child: Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: const [
+                SmallDepositButton(),
+                SizedBox(height: 12),
+                SmallWithdrawButton(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
